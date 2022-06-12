3ce56e024cc755fb8b3511875dc58986809f04da
Zachary <zacharylampi96@gmail.com>
2022-06-12 16:48:10 -0400

    commit tree 304f6e5d6258a119a728f5532626a0b353f2efeb

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction (cb) {return cb ()}
+function cb () {return "asdfasdf"}
+
+function returnsAnAnonymousFunction () {return function () {
+    (`Nom nom nom, this is delicious!`);
+  }}
+  
+function returnsANamedFunction () { return car}
+function car() {return "dont expect"}
+
+
